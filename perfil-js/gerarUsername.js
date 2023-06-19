611c0cdb8751cba39aea55393dda7b1b52641447
medina157 <102966250+medina157@users.noreply.github.com>
2023-06-19 12:00:48 -0300

    Add files via upload

---
+++ perfil-js/gerarUsername.js
@@ -0,0 +1,5 @@
+let nomeCompleto = String(document.getElementById('nomeCompleto').innerText)
+
+let nomeDeUsuario = document.getElementById('nomeDeUsuario')
+
+nomeDeUsuario.innerHTML = "@" + `${nomeCompleto.substring(0, 4)}213ZqWp09`
